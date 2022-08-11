6de51d95c090f3413ae1c018b4b062b6f7b3528d
Thomas Castiello <98052700+thomascastiello@users.noreply.github.com>
2022-08-11 18:24:05 -0300

    Detalle Del Producto

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import NavBar from './components/NavBar';
-import ItemListC from './components/ItemList/ItemListC';
+/* import ItemListC from './components/ItemList/ItemListC'; */
+import ItemDetail from './components/ItemDetail/ItemDetail';
+
 
 function App() {
   return (
@@ -10,7 +12,8 @@ function App() {
 
       <NavBar />
       <div className='mt-5'>
-      <ItemListC greeting=" Golden" />
+     {/*  <ItemListC greeting=" Golden" /> */}
+      <ItemDetail/>
       </div>
     </div>
 
